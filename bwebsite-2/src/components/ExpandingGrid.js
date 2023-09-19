7fa298810857867b4e2c0260398339b17b393c0a
Surya Gunasekaran <surya.gunasekaran@icloud.com>
2023-09-19 12:33:33 +0800

    done done

--- bwebsite-2/src/components/ExpandingGrid.js
+++ bwebsite-2/src/components/ExpandingGrid.js
@@ -19,21 +19,45 @@ const ExpandingGrid = () => {
       thumbnail: img2,
     },
     { id: 2,
-      name: "Item 2",
+      name: t('mea'),
       imageUrl: img,
-      description: "This is a brief description of Item 1.",
+      description: t('mead'),
       thumbnail: img2,
     },
     { id: 3,
-      name: "Item 3",
+      name: t('rlc'),
       imageUrl: img,
-      description: "This is a brief description of Item 1.",
+      description: t('rlcd'),
       thumbnail: img2,
     },
-        { id:   4,
-      name: "Item 4",
+    { id:   4,
+      name: t('mms'),
       imageUrl: img,
-      description: "This is a brief description of Item 1.",
+      description: t('mmsd'),
+      thumbnail: img2,
+    },
+    { id:   5,
+      name: t('pi'),
+      imageUrl: img,
+      description: t('pid'),
+      thumbnail: img2,
+    },
+    { id:   6,
+      name: t('ssw'),
+      imageUrl: img,
+      description: t('sswd'),
+      thumbnail: img2,
+    },
+    { id:   7,
+      name: t('ms'),
+      imageUrl: img,
+      description: t('msd'),
+      thumbnail: img2,
+    },
+    { id:   8,
+      name: t('ism'),
+      imageUrl: img,
+      description: t('ismd'),
       thumbnail: img2,
     },
     // ... add more items if needed ...
@@ -49,16 +73,99 @@ const ExpandingGrid = () => {
         return (
           <>
             <h2> {t('eg')} </h2>
-            <p> {t('egde')} </p>
+            <ul>
+              <li>{t('egde1')} <br/> </li>
+              <li>{t('egde2')} <br/> </li>
+              <li> {t('egde3')} <br/> </li>
+              <li>{t('egde4')} <br/> </li>     
+            </ul>
           </>
         );
-      case "Item 2":
+      case 2:
         return (
           <>
-            <h2>Details for Item 2</h2>
-            <p>Specific content for Item 2...</p>
+            <h2> {t('mea')} </h2>
+            <ul>
+              <li>{t('mea1')} <br/> </li>
+              <li>{t('mea2')} <br/> </li>
+              <li> {t('mea3')} <br/> </li>
+              <li>{t('mea4')} <br/> </li>     
+            </ul>
           </>
         );
+      case 3:
+        return (
+          <>
+            <h2> {t('rlc')} </h2>
+            <ul>
+              <li>{t('rlc1')} <br/> </li>
+              <li>{t('rlc2')} <br/> </li>
+              <li> {t('rlc3')} <br/> </li>
+              <li>{t('rlc4')} <br/> </li>     
+            </ul>
+          </>
+        );
+      case 4:
+        return (
+          <>
+            <h2> {t('mms')} </h2>
+            <ul>
+              <li>{t('mms1')} <br/> </li>
+              <li>{t('mms2')} <br/> </li>
+              <li> {t('mms3')} <br/> </li>
+              <li>{t('mms4')} <br/> </li>  
+              <li>{t('mms5')} <br/> </li>   
+            </ul>
+          </>
+        );
+      case 5:
+        return (
+          <>
+            <h2> {t('pi')} </h2>
+            <ul>
+              <li>{t('pi1')} <br/> </li>
+              <li>{t('pi2')} <br/> </li>
+              <li> {t('pi3')} <br/> </li>
+              <li>{t('pi4')} <br/> </li>  
+              <li>{t('pi5')} <br/> </li>   
+            </ul>
+          </>
+        );
+      case 6:
+        return (
+          <>
+            <h2> {t('ssw')} </h2>
+            <ul>
+              <li>{t('ssw1')} <br/> </li>
+              <li>{t('ssw2')} <br/> </li>
+              <li> {t('ssw3')} <br/> </li>
+              <li>{t('ssw4')} <br/> </li>     
+            </ul>
+          </>
+        );
+      case 7:
+        return (
+          <>
+            <h2> {t('ms')} </h2>
+            <ul>
+              <li>{t('ms1')} <br/> </li>
+              <li>{t('ms2')} <br/> </li>
+              <li> {t('ms3')} <br/> </li>
+              <li>{t('ms4')} <br/> </li>   
+            </ul>
+          </>
+        );
+      case 8:
+        return (
+          <>
+            <h2> {t('ism')} </h2>
+            <ul>
+              <li>{t('ism1')} <br/> </li>
+              <li>{t('ism2')} <br/> </li>
+            </ul>
+          </>
+        );
+
       // ... add more cases for other items as needed ...
       default:
         return null;
